830cb4ed20d3450f8930b50dfb556f76dd023c29
penpen09 <ar-el.mbl.10@outlook.jp>
2020-08-26 12:10:06 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'blogs/index'
   # get '/blogs', to:'blogs#index'#blogsコントローラのindexアクションを起動する
   # get '/stocks', to:'stocks#index'
-  root :to 'blogs#index'
+  root :to 'blogs#show'
   resources :blogs do
     collection do
       post :confirm
